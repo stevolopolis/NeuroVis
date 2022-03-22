09d7a1d2bb391c49fffcc2d7e1b49885b654420f
stevolopolis <stevenluots@gmail.com>
2022-03-21 23:01:44 -0400

    added path.spy.

---
+++ paths.py
@@ -0,0 +1,30 @@
+"""
+This file contains the Path class that creates, deletes, or
+modifies paths within the directory for saving AM results and
+other related images.
+
+This file is Copyright (c) 2022 Steven Tin Sui Luo.
+"""
+
+import os
+
+from parameters import GrParams
+
+params = GrParams()
+
+class GrPath:
+    """This class prepares the directories for saving AM results
+    in the <vis> folder.
+    """
+    def __init__(self, net: str):
+        self.net = net
+        self.main_path = params.PATH
+
+        # Create directory for currect experiment, if not already existing.
+        if self.main_path not in os.listdir('vis'):
+            os.makedirs(os.path.join('vis', self.main_path))
+    
+    def create_layer_paths(self, layer: str):
+        """This method creates sub-directory in <vis> for a specific layer."""
+        if layer not in os.listdir(os.path.join('vis', self.main_path)):
+            os.makedirs(os.path.join('vis', self.main_path, layer))
