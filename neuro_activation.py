fe6db374e109df66e0111bd3c3657319c7b00c56
stevolopolis <stevenluots@gmail.com>
2022-06-02 06:21:18 -0400

    Complete base code for neuron act. map.

--- neuro_activation.py
+++ neuro_activation.py
@@ -1,13 +1,10 @@
 """
-This code applies activation maximization (AM) on CNN kernels
-to visualize the specialization of each kernel.
+This code visualizes the neuron activation maps of specified kernels
+in a CNN, which aids our understanding of the purposes of each
+individual neuron in the neural network.
 
-Available AM losses and regularizers include:
-    - mean-of-feature
-    - mean-of-feature-pixel
-    - TV
-    - Jitter
-    - LP
+The activation maps are generated from the Cornell Dataset.
+The images are saved in the <datasets> folder.
 
 This file is Copyright (c) 2022 Steven Tin Sui Luo.
 """
@@ -17,10 +14,10 @@ import cv2
 from parameters import GrParams
 from paths import GrPath
 from models import ExtractModel
-from am import ActivationMaximization
-from utils import am_img_mat
+from data import DataLoader
+from utils import tensor2array, array2img
 
-# Params class containing parameters for AM visualization.
+# Params class containing parameters for all visualization.
 params = GrParams()
 # Path class for managing required directories
 # gr-convnet / resnet18 / vgg16
@@ -31,6 +28,9 @@ paths.create_act_path()
 model = torch.load(params.MODEL_PATH, map_location=params.DEVICE)
 model.eval()
 
+# DataLoader for Cornell Dataset
+dataLoader = DataLoader()
+
 # AM visualization
 for vis_layer in params.vis_layers:
     print('Visualizing for %s layer' % vis_layer)
@@ -39,10 +39,17 @@ for vis_layer in params.vis_layers:
 
     # AM on individual kernels
     for kernel_idx in range(params.N_KERNELS):
-        save_img_path = '%s/%s_%s.png' % (paths.save_subdir, vis_layer, kernel_idx)
-
+        print('Visualizing Kernel INDEX: %s' % kernel_idx)
         # Create submodel with output = selected kernel
         ext_model = ExtractModel(model, vis_layer, net_type=params.net, device=params.DEVICE)
         # Load Cornell Dataset images
-        # Feed image model and get act. map
-        # Save act. map
+        for img, img_id in dataLoader.load_rgbd():
+            save_img_path = '%s/%s_%s_%s.png' % (paths.save_subdir, img_id, vis_layer, kernel_idx)
+            # Feed image model and get act. map
+            output = ext_model(img)
+            act_map = output[0, kernel_idx]
+            # Save act. map
+            act_map_arr = tensor2array(act_map)
+            act_img = array2img(act_map_arr)
+            print(act_img.shape)
+            cv2.imwrite(save_img_path, act_img)
