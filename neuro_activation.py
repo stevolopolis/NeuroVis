dd6a9f8411be9f785601e1a1aeea043dd0f5f100
stevolopolis <stevenluots@gmail.com>
2022-06-02 05:40:56 -0400

    File for generating neuron act. maps.

---
+++ neuro_activation.py
@@ -0,0 +1,48 @@
+"""
+This code applies activation maximization (AM) on CNN kernels
+to visualize the specialization of each kernel.
+
+Available AM losses and regularizers include:
+    - mean-of-feature
+    - mean-of-feature-pixel
+    - TV
+    - Jitter
+    - LP
+
+This file is Copyright (c) 2022 Steven Tin Sui Luo.
+"""
+import torch
+import cv2
+
+from parameters import GrParams
+from paths import GrPath
+from models import ExtractModel
+from am import ActivationMaximization
+from utils import am_img_mat
+
+# Params class containing parameters for AM visualization.
+params = GrParams()
+# Path class for managing required directories
+# gr-convnet / resnet18 / vgg16
+paths = GrPath('gr-convnet')
+paths.create_act_path()
+
+# Load trained gr-convnet model
+model = torch.load(params.MODEL_PATH, map_location=params.DEVICE)
+model.eval()
+
+# AM visualization
+for vis_layer in params.vis_layers:
+    print('Visualizing for %s layer' % vis_layer)
+    # Create sub-directory for chosen layer
+    paths.create_layer_paths(vis_layer)
+
+    # AM on individual kernels
+    for kernel_idx in range(params.N_KERNELS):
+        save_img_path = '%s/%s_%s.png' % (paths.save_subdir, vis_layer, kernel_idx)
+
+        # Create submodel with output = selected kernel
+        ext_model = ExtractModel(model, vis_layer, net_type=params.net, device=params.DEVICE)
+        # Load Cornell Dataset images
+        # Feed image model and get act. map
+        # Save act. map
